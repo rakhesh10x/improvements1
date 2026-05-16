30fd1cfa9c449bce8c8595e964aeee5dfd29fb50
Rakhesh <rakhesh@example.com>
2026-05-16 10:37:01 +0530

    style: use Inter font for hero logo to ensure clean 'l' character rendering

--- src/components/Logo10X.jsx
+++ src/components/Logo10X.jsx
@@ -10,7 +10,7 @@ const Logo10X = ({ className }) => (
     <g transform="translate(582.733 378)">
       <text 
         fill="#FFFFFF" 
-        fontFamily="REM, REM_MSFontService, sans-serif" 
+        fontFamily="'Inter', sans-serif" 
         fontWeight="400" 
         fontSize="42.6667"
         textRendering="geometricPrecision"
