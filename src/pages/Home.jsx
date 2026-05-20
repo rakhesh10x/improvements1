e20373e6f8e9c3cac3dc780a8639b65476384955
Rakhesh <rakhesh@example.com>
2026-05-20 18:54:18 +0530

    style: globally standardize vertical spacing around all homepage dotted separators

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
         <TechnicalFiller />
 
         <FeatureLinks mode="home" />
-        <TechnicalFiller className="my-4 md:my-6" />
+        <TechnicalFiller />
 
         <PageGateways />
         <TechnicalFiller />
